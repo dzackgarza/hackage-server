b97c1ce3ebf2d0707c05bb9f1dab9295bf6cf998
Zack Garza <dzackgarza@gmail.com>
2015-06-02 04:44:37 +0000

    Completed fully-functioning "trivial example" of a new feature.
    Provides a resource endpoint at /packages/vote that returns a simple
    string.
    
    To test, use the following command:
    
    curl -i -H "Accept: application/json" -H "Content-Type:
    application/json" http:localhost:8080/packages/vote -L
    
    This will return the string "Here's a One!"

--- Distribution/Server/Features/Ranking.hs
+++ Distribution/Server/Features/Ranking.hs
@@ -2,7 +2,7 @@
 
 module Distribution.Server.Features.Ranking (
   RankingFeature(..),
-  initRankingFeature,
+  initRankingFeature
   ) where
 
 import Distribution.Server.Framework
@@ -21,63 +21,69 @@ type VoteTally = Int
 type UniquePackageNames = Set PackageName
 type VoteMap = Map UniquePackageNames VoteTally
 
+-- | Define the prototype for this feature
 data RankingFeature = RankingFeature {
-  rankingFeatureInterface :: HackageFeature
+  {-votesNum :: Int-}
+  rankingFeatureInterface :: HackageFeature,
 
-  -- getVoteResource :: Resource,
+  getVoteResource :: Resource
   -- May need to define a custom resource and branch it out, see TagsResource
 
   -- queryGetPackagesByVotes      :: MonadIO m => m [(PackageName, Int)],
   -- queryGetSinglePackageVotes   :: MonadIO m => PackageName -> m voteTally
-
 }
 
+-- | Implement the isHackageFeature 'interface'
 instance IsHackageFeature RankingFeature where
   getFeatureInterface = rankingFeatureInterface -- rankingInterface -- #ToDo: Define rankingInterface.
 
--- initRankingFeature :: ServerEnv
--- --> IO ()
-initRankingFeature :: ServerEnv
-                   -> IO (IO RankingFeature)
-initRankingFeature = error "initRankingFeature"
+-- | Called from Features.hs to initialize this feature
+initRankingFeature :: ServerEnv -> IO ( IO RankingFeature)
+initRankingFeature ServerEnv{serverStateDir} = do
+  return $ do
+    let feature = rankingFeature
+    return feature
 
--- Returns the number of votes a single package has.
+-- | Default constructor for building a feature.
+rankingFeature :: RankingFeature
+
+rankingFeature
+  = RankingFeature{..}
+  where
+    rankingFeatureInterface = (emptyHackageFeature "ranking") {
+        featureResources        = [getVoteResource]
+        , featureState          = []
+        , featureCaches         = []
+      }
+
+    getVoteResource = (resourceAt "/packages/vote") {
+          resourceDesc = [(GET, "Get the number of votes a package has.")],
+          resourceGet  = [("json", returnOne)]
+      }
+
+-- | Trivial json response
+returnOne :: DynamicPath -> ServerPartE Response
+returnOne d = ok . toResponse $ "Here's a One!"
+
+
+-- | Returns the number of votes a single package has.
 queryGetNumberOfVotes :: (voteMap -> PackageName) -> voteMap -> Int
 queryGetNumberOfVotes = error "queryGetNumberofVotes"
 
--- Post an upvote to a package, increasing it's ranking by one.
--- (Each user should only be able to add one upvote to a given package)
+-- | Post an upvote to a package, increasing it's ranking by one.
+-- | (Each user should only be able to add one upvote to a given package)
 putUpVote :: (voteMap -> PackageName) -> voteMap
 putUpVote = error "putUpVote"
 
--- Revoke a previous upvote
--- (Should only work if a user has previously +1'd a package)
+-- | Revoke a previous upvote
+-- | (Should only work if a user has previously +1'd a package)
 putRemoveVote :: (voteMap -> PackageName) -> voteMap
 putRemoveVote = error "putRemoveVote"
 
--- Return a list of packages, sorted by votes (descending)
+-- | Return a list of packages, sorted by votes (descending)
 queryGetBestPackages :: voteMap -> [(PackageName, Int)]
 queryGetBestPackages = error "queryGetBestPackages"
 
-rankingFeature :: CoreFeature -> RankingFeature
-rankingFeature c
-  = RankingFeature{..}
-  where
-  rankingFeatureInterface = (emptyHackageFeature "ranking") {
-      featureResources        = []
-      , featureState          = []
-      , featureCaches         = []
-      , featurePostInit       = error "featurePostInit"
-      }
-
--- searchFeatureInterface = (emptyHackageFeature "ranking") {
-  --  featureResources    = []
-  --, featureState        = []
-  --, featureCaches       = [
-  --      CacheComponent {
-  --        cacheDesc       = "package ranking system",
-  --        getCacheMemSize = memSize <$> readMemState searchEngineState
-  --      }
-  --    ]
-  -- , featurePostInit = unknown -- #ToDo
---}
+-- | Add a new package to the global map
+addNewPackage :: (voteMap -> PackageName) -> voteMap
+addNewPackage = error "addNewPackage"
