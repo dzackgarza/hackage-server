36ba94258df46fff9abda30cd33bb9ac69fc0042
Zack Garza <dzackgarza@gmail.com>
2015-06-04 02:26:43 +0000

    Add in-memory map, able to query via json.

--- Distribution/Server/Features/Ranking.hs
+++ Distribution/Server/Features/Ranking.hs
@@ -14,23 +14,24 @@ import Distribution.Package
 import Distribution.Server.Packages.Types
 
 import qualified Data.HashMap.Strict as HashMap
-import Data.Aeson (Value(..), toJSON)
+import Data.Aeson
 import qualified Data.Vector as Vector
 import Data.Map as Map
 import Data.Set as Set
 import Data.Text as Text
 
+import Control.Monad.Reader
 
-type VoteTally = Int
+type VoteCount = Integer
+type Name = String
 type UniquePackageNames = Set PackageName
-type VoteMap = Map UniquePackageNames VoteTally
+type VoteMap = Map String Integer
 
 -- | Define the prototype for this feature
 data RankingFeature = RankingFeature {
   {-votesNum :: Int-}
-  rankingFeatureInterface :: HackageFeature,
+  rankingFeatureInterface :: HackageFeature
 
-  getVoteResource :: Resource
   -- May need to define a custom resource and branch it out, see TagsResource
 
   -- queryGetPackagesByVotes      :: MonadIO m => m [(PackageName, Int)],
@@ -44,34 +45,55 @@ instance IsHackageFeature RankingFeature where
 -- | Called from Features.hs to initialize this feature
 initRankingFeature :: ServerEnv -> IO ( IO RankingFeature)
 initRankingFeature ServerEnv{serverStateDir} = do
+  initialVoteCache <- newMemStateWHNF Map.empty
   return $ do
-    let feature = rankingFeature
+    let feature = rankingFeature initialVoteCache
     return feature
 
 -- | Default constructor for building a feature.
-rankingFeature :: RankingFeature
+rankingFeature :: MemState (Map String Integer)
+                -> RankingFeature
 
-rankingFeature
+rankingFeature votesCache
   = RankingFeature{..}
   where
     rankingFeatureInterface = (emptyHackageFeature "ranking") {
-        featureResources        = [getVoteResource]
-        , featureState          = []
-        , featureCaches         = []
+      featureResources        = [getVoteResource]
+      , featurePostInit       = performInitProcedure
+      , featureState          = []
+      , featureCaches         = [
+          CacheComponent {
+            cacheDesc       = "package vote counts",
+            getCacheMemSize = memSize <$> readMemState votesCache
+          }
+        ]
       }
 
+    performInitProcedure = do
+      items <- constructInitialMap
+      writeMemState votesCache items
+
+    -- | Resources passed to featureResources in constructor
     getVoteResource = (resourceAt "/packages/vote") {
-          resourceDesc = [(GET, "Get the number of votes a package has.")],
-          resourceGet  = [("json", returnOne)]
-      }
+      resourceDesc = [(GET, "Get the number of votes a package has.")],
+      resourceGet  = [("json", returnOne)]
+    }
 
--- | Trivial json response
-returnOne :: DynamicPath -> ServerPartE Response
-returnOne d = ok . toResponse $
-            toJSON arr
-              where arr = [ ("param1", 1 :: VoteTally)
-                          , ("param2", 2 :: VoteTally)
-                          ]
+    -- | Trivial json response
+    returnOne _ = do
+        theMap <- readMemState votesCache
+        let
+          arr = if "c" `Map.member` theMap
+            then [("inMap", "True")]
+            else [("inMap", "False")]
+
+        ok. toResponse $ toJSON arr
+
+constructInitialMap :: IO (Map String Integer)
+constructInitialMap = return initialTestMap
+
+initialTestMap :: VoteMap
+initialTestMap = Map.fromList [("a", 1), ("b", 2)] :: VoteMap
 
 -- | Returns the number of votes a single package has.
 queryGetNumberOfVotes :: (voteMap -> PackageName) -> voteMap -> Int
